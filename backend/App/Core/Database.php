a2c4dccd81ed8ac72ebaed3c9ee82e1054c0dd12
LuisF <luis-lipe@hotmail.com>
2022-12-08 11:12:46 -0300

    Changed DB_USER variable name

--- backend/App/Core/Database.php
+++ backend/App/Core/Database.php
@@ -9,9 +9,9 @@ class Database {
     public static function getConn(){
         var_dump($_ENV['HOST']);
         var_dump($_ENV['DB_NAME']);
-        var_dump($_ENV['USER']);
+        var_dump($_ENV['DB_USER']);
         if(!isset(self::$conexao)){
-            self::$conexao = new \PDO('mysql:host='.$_ENV['HOST'].'; dbname='.$_ENV['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
+            self::$conexao = new \PDO('mysql:host='.$_ENV['HOST'].'; dbname='.$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['PASSWORD']);
         }
 
         return self::$conexao;
