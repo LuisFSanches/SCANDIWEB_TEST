d5ee887d74cc73996839409618e4ea1ae807cbc9
LuisF <luis-lipe@hotmail.com>
2022-12-07 23:38:18 -0300

    Removed space from host name.

--- backend/App/Core/Database.php
+++ backend/App/Core/Database.php
@@ -8,7 +8,7 @@ class Database {
 
     public static function getConn(){
         if(!isset(self::$conexao)){
-            self::$conexao = new \PDO('mysql: host='.$_ENV['HOST'].'; dbname='.$_ENV['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
+            self::$conexao = new \PDO('mysql:host='.$_ENV['HOST'].'; dbname='.$_ENV['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
         }
 
         return self::$conexao;
