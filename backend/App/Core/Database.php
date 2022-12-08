99acc8ce30e53efc77bcf1edc9d3d95a89746381
LuisF <luis-lipe@hotmail.com>
2022-12-08 11:07:13 -0300

    Checking if environment variables are working

--- backend/App/Core/Database.php
+++ backend/App/Core/Database.php
@@ -7,6 +7,9 @@ class Database {
     private static $conexao;
 
     public static function getConn(){
+        var_dump($_ENV['HOST']);
+        var_dump($_ENV['DB_NAME']);
+        var_dump($_ENV['USER']);
         if(!isset(self::$conexao)){
             self::$conexao = new \PDO('mysql:host='.$_ENV['HOST'].'; dbname='.$_ENV['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
         }
