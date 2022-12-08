f0d7a4c4c82e6e50b8085c3159b75673f3a89d79
LuisF <luis-lipe@hotmail.com>
2022-12-07 23:35:33 -0300

    Added verification if env file exists.

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,9 +2,10 @@
 
 require_once("../vendor/autoload.php");
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
+if (file_exists('../.env')) {
+  $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+  $dotenv->load();
+} 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
