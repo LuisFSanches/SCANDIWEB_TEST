a0c056177026c3986d75a8acef7e3d15799ea720
LuisF <luis-lipe@hotmail.com>
2022-12-16 15:12:09 -0300

    Model type added

--- backend/App/Core/Controller.php
+++ backend/App/Core/Controller.php
@@ -4,17 +4,18 @@ namespace App\Core;
 
 class Controller{
 
-    public function model($model){
-        require_once "../App/Models/" . $model . ".php";
-        
-        return new $model;
-    }
+  public function model($model){
+    $lowerCaseType = strtolower($model);
+    $modelType = ucwords($lowerCaseType);
+    require_once "../App/Models/" . $modelType . ".php";  
+      
+    return new $model;
+  }
 
-    protected function getRequestBody(){
-        $json = file_get_contents("php://input");
-        
-        $obj = json_decode($json);
-        return $obj;
-    }
-
+  protected function getRequestBody(){
+    $json = file_get_contents("php://input");
+      
+    $obj = json_decode($json);
+    return $obj;
+  }
 }
